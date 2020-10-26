c6073c572ffb5207664712c3a6b42d384da0a2c5
dotnet noobie <dotnetnoobie@gmail.com>
2020-10-26 14:00:28 +1100

    Add Google Auth with YouTube access

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,6 +16,16 @@ namespace BlazorApp.Client
             var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                options.ProviderOptions.MetadataUrl = "https://accounts.google.com/.well-known/openid-configuration";
+                options.ProviderOptions.ResponseType = "id_token token";
+                options.ProviderOptions.Authority = "https://accounts.google.com";
+                options.ProviderOptions.PostLogoutRedirectUri = $"{builder.HostEnvironment.BaseAddress}/authentication/logout-callback";
+                options.ProviderOptions.ClientId = "10126427864-o1t3qcc9lgdr2lalclhu9h8170ea4dg9.apps.googleusercontent.com";
+                options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/youtube");
+            });
+
             await builder.Build().RunAsync();
         }
     }
